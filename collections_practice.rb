d2c71a277bcd685407810186b164f39b1cdec04b
Anton Kalyutich <anton.kalyutich@gmail.com>
2020-08-24 21:37:01 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -25,7 +25,7 @@ end
 def kesha_maker(strings)
   new_strings = []
   strings.each do |string|
-    new_string = string.split
+    string_array = string.split
     new_string[2] = "$"
     new_strings << new_string.join
   end
