cb21f382ba007c37c78b39be95f71429df1c25c5
Anton Kalyutich <anton.kalyutich@gmail.com>
2020-08-24 20:58:26 +0000

    Automatically backed up by Learn

--- spec/collections_practice_spec.rb
+++ spec/collections_practice_spec.rb
@@ -32,7 +32,7 @@ describe 'collections practice' do
   
   # Advanced #2
   describe '#swap_elements_from_to' do
-    it 'takes in three arguments (array, index and the new index) allows you to specify the index of the element you would like to move to a new index' do
+    it 'takes in three arguments (array, index and the new index), allows you to specify the index of the element you would like to move to a new index' do
       expect(swap_elements(["blake", "ashley", "scott"])).to eq(["blake", "scott", "ashley"])
     end
   end
