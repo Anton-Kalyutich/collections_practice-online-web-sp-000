38a74524bd04b2db258ed5124177c861fccec5e3
Anton Kalyutich <anton.kalyutich@gmail.com>
2020-08-24 20:56:40 +0000

    Automatically backed up by Learn

--- spec/collections_practice_spec.rb
+++ spec/collections_practice_spec.rb
@@ -31,7 +31,7 @@ describe 'collections practice' do
   end
   
   # Advanced #2
-  describe '#swap_elements' do
+  describe '#swap_elements_from_to' do
     it 'swap the second and third elements of an array' do
       expect(swap_elements(["blake", "ashley", "scott"])).to eq(["blake", "scott", "ashley"])
     end
